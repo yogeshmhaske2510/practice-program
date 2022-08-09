af7155ebfe5f9b8d380786ad22cd6b1a127c692b
yogesh mhaske <yogeshmhaske2510@gmail.com>
2022-08-09 18:53:14 +0530

    Created two pojo and Program to calculate Min, Max, Average, Sum, 2nd & 3rd latest and Join function of Java8

--- src/com/java8/GroupBy.java
+++ src/com/java8/GroupBy.java
@@ -1,5 +1,7 @@
 package com.java8;
 
+import com.java8.pojo.User;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -29,55 +31,5 @@ public class GroupBy {
         * */
         Map<Integer, Set<User>> uniqueAndSortedUserMap = users.stream().collect(Collectors.groupingBy(u-> u.getId(), TreeMap::new,  Collectors.toSet()));
         System.out.println(uniqueAndSortedUserMap);
-    }
-}
-
-class User {
-    private int id;
-    private String name;
-
-    public User() {
-    }
-
-    public User(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(name, user.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
     }
 }
